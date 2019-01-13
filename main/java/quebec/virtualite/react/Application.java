700ae56e74d2c9e862f5028abe66b7f52aa3c95b
pro@virtualite.quebec <ibNj14Y4KAKm4JMk>
2019-01-13 02:28:13 -0500

    Initial

---
+++ main/java/quebec/virtualite/react/Application.java
@@ -0,0 +1,41 @@
+package quebec.virtualite.react;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication(scanBasePackages =
+    {
+        "quebec.virtualite.*"
+    })
+public class Application extends SpringBootServletInitializer
+{
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    static
+    {
+        log.warn("STARTING...");
+    }
+
+    public static void main(final String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        log.warn("STARTED");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(Application.class);
+    }
+}
