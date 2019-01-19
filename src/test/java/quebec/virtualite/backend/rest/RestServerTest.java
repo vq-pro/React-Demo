3779a92a46f47c9045567ff979b9530602878eee
pro@virtualite.quebec <ibNj14Y4KAKm4JMk>
2019-01-18 19:44:15 -0500

    Fix for new Mockito

--- src/test/java/quebec/virtualite/backend/rest/RestServerTest.java
+++ src/test/java/quebec/virtualite/backend/rest/RestServerTest.java
@@ -3,7 +3,7 @@ package quebec.virtualite.backend.rest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import quebec.virtualite.backend.domain.Greeting;
 
 import static org.hamcrest.core.Is.is;
