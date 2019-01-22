972455475aa1ee9f403fdacee9559fc38cdef651
pro@virtualite.quebec <ibNj14Y4KAKm4JMk>
2019-01-22 14:47:47 -0500

    Version for web service

--- src/main/java/quebec/virtualite/backend/services/rest/RestServer.java
+++ src/main/java/quebec/virtualite/backend/services/rest/RestServer.java
@@ -3,12 +3,14 @@ package quebec.virtualite.backend.services.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import quebec.virtualite.backend.services.domain.Domain;
 
 import static java.lang.String.format;
 
 @RestController
+@RequestMapping("/v1")
 public class RestServer
 {
     @Autowired
