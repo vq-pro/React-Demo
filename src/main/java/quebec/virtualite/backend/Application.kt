8b319b134015d480882494e977044cd8daf13b07
Patrick Robert <pro@virtualite.quebec>
2020-08-27 13:59:35 -0400

    Kotlin

--- src/main/java/quebec/virtualite/backend/Application.kt
+++ src/main/java/quebec/virtualite/backend/Application.kt
@@ -2,8 +2,8 @@ package quebec.virtualite.backend
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import quebec.virtualite.backend.security.SecurityUserManager
 import quebec.virtualite.backend.security.SecurityUsers.TEST_PASSWORD
 import quebec.virtualite.backend.security.SecurityUsers.TEST_USER
@@ -17,24 +17,15 @@ open class Application
     @Autowired
     private lateinit var userManager: SecurityUserManager
 
-    companion object
-    {
-        @JvmStatic
-        fun main(args: Array<String>)
-        {
-            SpringApplication.run(Application::class.java, *args)
-        }
-
-        init
-        {
-            log.warn("STARTING...")
-        }
-    }
-
     @PostConstruct
     fun init()
     {
         userManager.defineUser(TEST_USER, TEST_PASSWORD)
         log.warn("STARTED")
     }
+}
+
+fun main(args: Array<String>)
+{
+    runApplication<Application>(*args)
 }
